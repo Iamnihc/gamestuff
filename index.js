2a56239034a28dbff20e294598ef7018da250c77
Iamnihc <34426008+Iamnihc@users.noreply.github.com>
2020-06-30 21:09:21 -0700

    initial

---
+++ index.js
@@ -0,0 +1,36 @@
+const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
+
+function room(number, type="empty", players = []){
+
+    return (number, type, players)
+}
+
+var rooms = [room(1)];
+
+
+
+
+
+const httpServer = require('http').createServer((req, res) => {
+  // serve the index.html file
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(content));
+  res.end(content);
+});
+
+const io = require('socket.io')(httpServer);
+
+io.on('connect', socket => {
+  console.log('connect');
+  socket.emit()
+});
+
+httpServer.listen(3000, () => {
+  console.log('go to http://localhost:3000');
+});
+
+io.on('connect', socket => {
+      socket.emit('setup', "Test server");
+
+  });
+
