dcde42080341218a4d55c9dd34753af9d5fcc342
Iamnihc <8ndf73@gmail.com>
2020-07-01 19:59:35 -0700

    aaa

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const servername = "TEST SEVER"
 var fs = require('fs');
 const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
 
-var users = []
+var users = syncUserBase();
 
 function syncUserBase(){
-  var users = fs.readFileSync('./config.json'),
-    myObj;
-
+  var data = fs.readFileSync('./users.json');
   try {
-    myObj = JSON.parse(data);
-    console.dir(myObj);
+    users = JSON.parse(data);
+    console.dir(users);
+    return users;
   }
   catch (err) {
     console.log('There has been an error parsing your JSON.')
     console.log(err);
+    return [];
   }
 }
 
@@ -23,7 +23,7 @@ function saveUserBase(){
 
   fs.writeFile('./users.json', data, function (err) {
     if (err) {
-      console.log('There has been an error saving your configuration data.');
+      console.log('There has been an error saving your user data.');
       console.log(err.message);
       return;
     }
@@ -37,7 +37,7 @@ function room(number, type="empty", players = []){
 }
 
 function user(name, pin=1234){
-  return(name,pin);
+  return( {name,pin} );
 }
 
 var rooms = [room(1)];
@@ -58,15 +58,26 @@ io.on('connect', socket => {
   socket.emit('setup', servername + "player number "+ playernum);
   socket.on('user', data => {
     console.log("trying to login as " + data);
-    if (users.includes(data)){
+    if (users.some(x => x.name == data)){
       socket.emit('pin',data);
     }
     else{
-      socket.emit('getpin', data)
+      socket.emit('makepin', data)
     }
   });
-
-  
+  socket.on('usercreate', data =>{
+    users.push(user(data.uname, data.pin));
+    console.log(users);
+    console.log(data);
+    saveUserBase();
+  });
+  socket.on('auth', data =>{
+    console.log(users);
+    let loginuser = user(data.uname, data.pin);
+    if (users.includes(loginuser)){
+      console.log("logged in as " + loginuser.name);
+    }
+  })
 });
 
 
